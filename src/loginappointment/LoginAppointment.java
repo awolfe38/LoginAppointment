09ea496a349b9c47bd060cb87f3cd379ab48b1f5
Alex Wolfe <ajw5138@gmail.com>
2021-03-11 02:26:43 -0500

    --

--- src/loginappointment/LoginAppointment.java
+++ src/loginappointment/LoginAppointment.java
@@ -5,35 +5,61 @@
  */
 package loginappointment;
 
+import java.io.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.sql.Connection;
+import java.util.Scanner;
 import utils.DBConnection;
+import model.User;
 
 /**
  *
  * @author ajw51
  */
+/**This class creates an app that connects to a MySQL database. There are 2 additional lambdas here in the main method.*/
 public class LoginAppointment extends Application {
 
     /**
-     * @param args the command line arguments
+     * This is the main method.The first method that gets called. There are two lambdas here that keep track and store all of the successful and failed login attempts.
+     * @param args --.
+     * @throws java.io.FileNotFoundException File Not Found Exception
+     * 
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        
+    String filename = "login_activity.txt";
+    Scanner keyboard = new Scanner(System.in);
+    
+    FileWriter fwriter = new FileWriter(filename, true);
+    PrintWriter outputFile = new PrintWriter(fwriter);
         
         Connection conn = DBConnection.startConnection();
         
         launch(args);
         
+        /**Lambda that keeps track and stores all of the successful login attempts. */
+        PrintInterface message = s -> "Login Successes: " + s;
+        outputFile.println(message.getMessage(User.getLog()));
+       
+        
+        /**Lambda that keeps track and stores all of the failed login attempts. */
+        FailedInterface failmessage = f -> "Login Fails: " + f;
+        outputFile.println(failmessage.failmessage(User.failLog));
+        
+        outputFile.close();
+        System.out.println("printed");
+        
+        
         DBConnection.closeConnection();
     }
     
         @Override
-    //Pulling up the Main Menu
+    /**Pulling up the Main Menu. */
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/view/LoginMenu.fxml"));
         Scene scene = new Scene(root);
